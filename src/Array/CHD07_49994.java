2142c7bd4b2ee9a2fb9976a92cd97f4eea76a581
ps202203 <pase375@gmail.com>
2025-12-02 22:02:58 +0900

    CHD07_49994.java

---
+++ src/Array/CHD07_49994.java
@@ -0,0 +1,47 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class CHD07_49994 {
+    public static void main(String[] args) {
+        String dirs = "ULURRDLLU";
+        int result = solution(dirs);
+        System.out.println(result);
+    }
+    private static boolean isValidMove(int nx, int ny) {
+        return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
+    }
+
+    private static final HashMap<Character, int[]> location = new HashMap<>();
+
+    private static void initLocation() {
+        location.put('U', new int[]{0, 1});
+        location.put('D', new int[]{0, -1});
+        location.put('L', new int[]{-1, 0});
+        location.put('R', new int[]{1, 0});
+    }
+
+    public static int solution(String dirs) {
+        initLocation();
+        int x = 5, y = 5;
+        HashSet<String> answer = new HashSet<>();
+
+        for (int i = 0; i < dirs.length(); i++) {
+            int[] offset = location.get(dirs.charAt(i));
+            int nx = x + offset[0];
+            int ny = y + offset[1];
+            if (!isValidMove(nx, ny))
+                continue;
+
+            answer.add(x + " " + y + " " + nx + " " + ny);
+            answer.add(nx + " " + ny + " " + x + " " + y);
+
+            x = nx;
+            y = ny;
+        }
+
+        return answer.size() / 2;
+    }
+
+}
